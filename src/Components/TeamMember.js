4365bb23f1c0c6129cd6f18e9f4304f73fcf6f50
Ishita Arora <iwinner21@gmail.com>
2022-09-19 14:44:13 +0530

    React Basics

---
+++ src/Components/TeamMember.js
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const TeamMember = ({selectedTeam,male,female,employee,handleEmployeeCardClick}) => {
+  return (
+    <div
+                key={employee.id}
+                id={employee.id}
+                className={(employee.teamName === selectedTeam? 'card m-2 standout': 'card m-2')}
+                style={{ cursor: "pointer" }}
+                onClick={handleEmployeeCardClick}
+              >
+                {employee.gender === "male" ? (
+                  <img src={male} className="card-img-top" />
+                ) : (
+                  <img src={female} className="card-img-top" />
+                )}
+                <div className="card-body">
+                  <h5 className="card-title">
+                    Full Name : {employee.fullName}
+                  </h5>
+                  <p className="card-text">
+                    <b>Designation: </b>
+                    {employee.designation}
+                  </p>
+                </div>
+              </div>
+  )
+}
+
+export default TeamMember
